9fab44d75f087240759d1090f1761894d42eab7e
eltlutfi <lutfi200499@gmail.com>
2022-04-08 23:54:01 +0700

    initial commit

---
+++ multiple-page.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const server = http.createServer((request, response) => {
+    const requestUrl = url.parse(request.url).pathname;
+
+    if (requestUrl === "/") {
+    fs.readFile("./index.html", null, (error, data) => {
+        if (error) {
+            response.writeHead(404);
+            response.write("not found");
+        } else {
+            response.writeHead(200, { "Content-Type": "text/html" });
+            response.write(data);
+        }
+
+        response.end();
+    });
+
+    } else if (requestUrl === "/device") {
+    fs.readFile("./device.json", null, (error, data) => {
+        if (error) {
+            response.writeHead(404);
+            response.write("not found");
+        } else {
+            response.writeHead(200, { "Content-Type": "text/html" });
+            response.write(data);
+        }
+
+        response.end();
+    });
+    } else {
+        response.writeHead(404);
+        response.write("not found");
+        response.end();
+    }
+});
+
+server.listen(8000);
